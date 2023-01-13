50c252f3813275ffb1cc5d5617e08070c4b31549
PotatoChip1 <ejvandergoot@gmail.com>
2023-01-13 13:15:07 -0500

    added header file

--- code/new-beautiful-creations/sub-new/sub-new.ino
+++ code/new-beautiful-creations/sub-new/sub-new.ino
@@ -8,52 +8,7 @@
 #include "Wire.h"
 #endif
 
-//#define USE_OLD_PINS //old board
-#define USE_NEW_PINS //board revision 1
-
-#define USE_MAGNET_JOYSTICK
-//#define ENABLE_AUTO_PUMPS
-
-//#define DEBUG
-
-#define arraylength(x) (sizeof(x) / sizeof((x)[0]))
-
-#ifdef USE_OLD_PINS
-#define INTERRUPT_PIN 2
-#define UP_BUTTON_PIN 4
-#define DOWN_BUTTON_PIN A1
-#define MODE_BUTTON_PIN A0
-#define EXTRA_BUTTON_1_PIN 13
-#define DEPTH_LED_PIN 0
-#define ACTUATOR_A_PIN 8
-#define ACTUATOR_B_PIN 7
-#define PUMP_A_PIN 11
-#define PUMP_B_PIN 10
-#define JOYSTICK_X_PIN A2
-#define JOYSTICK_Y_PIN A3
-#define SERVO_1_PIN 9
-#define SERVO_2_PIN 6
-#define SERVO_3_PIN 5
-#define SERVO_4_PIN 3
-#endif
-
-#ifdef USE_NEW_PINS
-#define UP_BUTTON_PIN A0
-#define DOWN_BUTTON_PIN 13
-#define MODE_BUTTON_PIN 8
-#define EXTRA_BUTTON_1_PIN 7
-#define DEPTH_LED_PIN 12
-#define ACTUATOR_A_PIN 13
-#define ACTUATOR_B_PIN A0
-#define PUMP_A_PIN 2
-#define PUMP_B_PIN 4
-#define JOYSTICK_X_PIN A2
-#define JOYSTICK_Y_PIN A3
-#define SERVO_1_PIN 9
-#define SERVO_2_PIN 6
-#define SERVO_3_PIN 5
-#define SERVO_4_PIN 3
-#endif
+#include "shared/submarine.h"
 
 Servo servos[4];
 
