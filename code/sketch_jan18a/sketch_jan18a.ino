4c3c338e7a768838edbbaaf7ad68bcf09946a006
PotatoChip1 <ejvandergoot@gmail.com>
2023-01-18 19:36:41 -0500

    finished led example

--- code/sketch_jan18a/sketch_jan18a.ino
+++ code/sketch_jan18a/sketch_jan18a.ino
@@ -1,25 +1,44 @@
-
 long currentTime, previousTime;
+boolean isOn;
+int blinkTime;
+
+//#define SLOW_BLINK_TIME 1000
+#define FAST_BLINK_TIME 250
+//#define SOLID_LED 0
 
 void setup() {
   Serial.begin(9600);
   pinMode(12, OUTPUT);
 
   previousTime = millis();
+  isOn = false;
 
+  #ifdef SLOW_BLINK_TIME 
+  blinkTime = SLOW_BLINK_TIME;
+  #endif
+
+  #ifdef FAST_BLINK_TIME
+  blinkTime = FAST_BLINK_TIME;
+  #endif
+
+  #ifdef SOLID_LED
+  blinkTime = SOLID_LED;
+  #endif
 }
 
 void loop() {
   currentTime = millis();
 
-  if(currentTime > previousTime + 1) {
+  if(currentTime > previousTime + blinkTime) {
 
-    if(digitalRead(12) == LOW){
-      digitalWrite(12, HIGH);
-    } else {
+    if(isOn){
       digitalWrite(12, LOW);
+      isOn = false;
+    } else {
+      digitalWrite(12, HIGH);
+      isOn = true;
     }
-    previousTime == currentTime;  
+    previousTime = currentTime; 
+     
   }
-  
 }
